30e44478d19d08c52bbc66958adbc27de8a19b9a
zhixian001 <okyjh12@naver.com>
2022-03-25 20:15:46 +0900

    add polyfill

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = function override(config, env) {
+  config.plugins = [...config.plugins, new NodePolyfillPlugin()];
+
+  return config;
+};
